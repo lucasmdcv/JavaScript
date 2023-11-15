1a7a40ef285b7a063f9bc535f998a17a48dea4f0
Lucas Mendes <111598752+lucasmdcv@users.noreply.github.com>
2023-11-15 17:08:05 -0300

    Add files via upload

---
+++ 07 - Listas (Arrays)/ExecutandoForEmUmaString.js
@@ -0,0 +1,9 @@
+
+//vamos percorrer todas as letras do nome 
+const nome = 'Lucas Mendes da Conceição Vieira';
+
+
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);
+}
---
+++ 07 - Listas (Arrays)/aula.js
@@ -0,0 +1,12 @@
+//Arrays são listas de Conteúdo
+
+//representadas por colchetes 
+const alunos = ['João', 'Vitor', 'Marina'];
+
+alunos.push('Renan');
+
+alunos[4] = 'Vinicios';
+
+alunos.pop();
+
+console.log(alunos);
