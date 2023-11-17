fd680c3584785e67c417e27ba104ab10e026f247
Lucas Mendes <111598752+lucasmdcv@users.noreply.github.com>
2023-11-17 02:51:57 -0300

    Update exercicio3.js

--- 09- Desafios de Códigos/exercicio3.js
+++ 09- Desafios de Códigos/exercicio3.js
@@ -20,3 +20,33 @@
 //             2050.00
 
 const { gets, print } = require('./funcoes-auxiliares-ex3');
+
+// variáveis de entrada
+const valorSalarioBruto = gets();
+const valorAdicionalDosBeneficios = gets();
+
+//  calculo de Porcentagem
+function calcularPorcentagem(valor,percentual ){
+    return valor * (percentual / 100) ;
+}
+
+
+//  pegarPercentualImpostoComBaseNoSalario
+function pegarPercentualImpostoComBaseNoSalario(salario){
+    
+    if (salario >= 0.00 && salario <= 1100.00) {
+        //return salario * (percentual / 5)
+        return 5.00;
+    } else if (salario >= 1100.01 && salario <= 2500){
+        return 10;
+    } else{
+        return 15;
+    }
+        
+
+}
+
+//  valorATransferir
+const valorATransferir = valorSalarioBruto - (calcularPorcentagem (valorSalarioBruto, pegarPercentualImpostoComBaseNoSalario(valorSalarioBruto))) + valorAdicionalDosBeneficios;
+
+print(valorATransferir);
